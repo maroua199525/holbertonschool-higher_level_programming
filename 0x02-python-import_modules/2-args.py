769807e5f71e1c2de1d2f4a5e3d1a61e641d57b6
maroua199525 <2646@holbertonschool.com>
2021-04-29 13:41:44 +0100

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv
-if (len(argv) - 1) != 1:
-    print("{:d} arguments:".format(len(argv) - 1))
-else:
-    print("{} argument:".format(len(argv) - 1))
-for i in range(0, len(argv)):
-    if (i > 0):
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    if (len(argv) - 1) != 1:
+        print("{:d} arguments:".format(len(argv) - 1))
+    else:
+        print("{} argument:".format(len(argv) - 1))
+    for i in range(0, len(argv)):
+        if (i > 0):
         print("{:d}: {}".format(i, argv[i]))
