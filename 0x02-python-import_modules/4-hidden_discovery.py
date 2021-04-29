ac11f4a635f795954dce98d6396f3a1ab814a2f9
maroua199525 <2646@holbertonschool.com>
2021-04-29 21:32:35 +0100

    fixed code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     hidden = dir(hidden_4)
-    for i in range(0, len(hidden)):
-        if hidden[i:i+2] != '__':
-            print("{}".format(hidden[i]))
+    for i in hidden:
+        if i[0:2] != '__':
+            print("{}".format(i))
