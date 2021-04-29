758b7a729212ec7dd726583603b4475b04cd64d0
maroua199525 <2646@holbertonschool.com>
2021-04-29 13:50:50 +0100

    code indent

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,12 @@
 if __name__ == "__main__":
     import sys
     argv = sys.argv
-    if (len(argv) - 1) != 1:
-        print("{:d} arguments:".format(len(argv) - 1))
+    if (len(argv) - 1) == 1:
+        print("{:d} argument:".format(len(argv) - 1))
+    elif (len(argv) - 1) == 0:
+        print("{:d} arguments.".format(len(argv) - 1))
     else:
-        print("{} argument:".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
     for i in range(0, len(argv)):
         if (i > 0):
             print("{:d}: {}".format(i, argv[i]))
