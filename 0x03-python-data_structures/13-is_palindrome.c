4dca8c315da5fa2e7dd02dd110e3cc1bb80a00ea
maroua199525 <2646@holbertonschool.com>
2021-05-04 21:37:09 +0100

    is palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - check the list is palindrome
+ * @head: pointer of pointer to list
+ * Return: 1 or 0
+ */
+int is_palindrome(listint_t **head)
+{
+	size_t i = 0, j = 0;
+	listint_t *tmp;
+	int array[1024];
+
+	if (*head == NULL)
+	{
+		return (1);
+	}
+	tmp = *head;
+	while (tmp != NULL)
+	{
+		array[i] = tmp->n;
+		i++;
+		tmp = tmp->next;
+	}
+	while (j < (i / 2))
+	{
+		if (array[j] != array[i - 1 - j])
+			return (0);
+		j++;
+	}
+	return (1);
+}
