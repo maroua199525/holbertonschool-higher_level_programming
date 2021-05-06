421e05a4362f647c7e82106c91fb26c77df0dbde
maroua199525 <2646@holbertonschool.com>
2021-05-06 17:16:45 +0100

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,12 +2,13 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    score = list((a_dictionary.values()))
-    key = list(a_dictionary.keys())
-    max = score[0]
-    name = key[0]
-    for i, v in zip(score, key):
-        if i > max:
-            max = i
-            name = v
-    return name
+    if len(a_dictionary.items()) >= 1:
+        score = list((a_dictionary.values()))
+        key = list(a_dictionary.keys())
+        max = score[0]
+        name = key[0]
+        for i, v in zip(score, key):
+            if i > max:
+                max = i
+                name = v
+        return name
