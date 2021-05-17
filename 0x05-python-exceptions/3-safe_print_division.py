426ca47eef530dfec1a69a3d6c2e6e3850f44225
maroua199525 <2646@holbertonschool.com>
2021-05-17 12:44:09 +0100

    add format()

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    c = 1
     try:
         c = (a / b)
     except ZeroDivisionError:
-        print("Inside result: None")
-        return (None)
+        c = None
     finally:
-        pass
-    print("Inside result:{}".format(c))
+        print("Inside result:{}".format(c))
     return (c)
