29bc4880d4ae1b23e91976125c89877210a5ff64
maroua199525 <2646@holbertonschool.com>
2021-05-17 14:38:18 +0100

    pep8 fixed + add chmod

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,5 +5,5 @@ def safe_function(fct, *args):
         result = fct(*args)
         return (result)
     except Exception as err:
-        print("Exception: {}".format(err), file = sys.stderr)
+        print("Exception: {}".format(err), file=sys.stderr)
         return None
