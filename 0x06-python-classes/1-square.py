5f229d29b4b178b7287146abe1c0ff178627b097
maroua199525 <2646@holbertonschool.com>
2021-05-18 16:24:08 +0100

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a sequare defines by size."""
+    def __init__(self, size):
+        """Initializes the private object."""
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a sequare defines by size."""
+    def __init__(self, size=0):
+        """Initializes the private object, sets size to zero then
+        checks if size has the correct type and the correct value"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
