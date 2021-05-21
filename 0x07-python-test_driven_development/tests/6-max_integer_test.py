4d2e2edd63f169c02e912549024f6fe941d985ef
maroua199525 <2646@holbertonschool.com>
2021-05-21 18:32:23 +0100

    remove wrong case

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,7 +9,6 @@ def max_integer(list=[]):
         self.assertEqual(max_integer(([1, 3, 6, 2], 6))
         self.assertEqual(max_integer([1, 7, 0, -3]), 7)
         self.assertEqual(max_integer([5, 7, 9, 8, 9, 1]), 9)
-        self.assertEqual(max_integer(""), None)
         self.assertEqual(max_integer([]), None)
         with self.assertRaises(TypeError):
             max_integer(["hello", 1, 2])
