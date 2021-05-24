a1f45d2da15bc7520efcc57d369848242106a444
maroua199525 <2646@holbertonschool.com>
2021-05-24 22:13:54 +0100

    changed message error

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -101,10 +101,11 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
+        symbol = str(self.print_symbol)
         new = ""
         for i in range(0, self.__height):
             for j in range(0, self.__width):
-                new += "#"
+                new += symbol
             if i != (self.__height - 1):
                 new += '\n'
         return new
