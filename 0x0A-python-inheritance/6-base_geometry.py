ff7c8ac7976b77cd3ca2ffff078d7e2f51fd5871
maroua199525 <2646@holbertonschool.com>
2021-06-01 12:39:33 +0100

    basegeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module BaseGeometry"""
+
+
+class BaseGeometry:
+    """ class BaseGeometry"""
+
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
