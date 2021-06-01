f596f169d762779350b1fb379e151db3df816a15
maroua199525 <2646@holbertonschool.com>
2021-06-01 16:57:51 +0100

    my int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module MyInt"""
+
+
+class MyInt(int):
+    def is_myint(a, b):
+        """ function is_myint
+        """
+        if a == b:
+            return False
+        else:
+            return True
