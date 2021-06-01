0f408b74ef6de4b633fbf5173734409f5aa5fda2
maroua199525 <2646@holbertonschool.com>
2021-06-01 17:08:11 +0100

    main to test files

---
+++ 0x0A-python-inheritance/test_file/1-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+MyList = __import__('1-my_list').MyList
+
+my_list = MyList()
+my_list.append("hi")
+my_list.append("school")
+my_list.append("holberton")
+my_list.append("team")
+print(my_list)
+my_list.print_sorted()
+print(my_list)
