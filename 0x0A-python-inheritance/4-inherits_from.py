f07213627c66601d93daa7544118df2234f911be
maroua199525 <2646@holbertonschool.com>
2021-06-01 18:06:16 +0100

    pep8  fixed

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,6 +2,7 @@
 """ Module contain function: inherits_from
 """
 
+
 def inherits_from(obj, a_class):
     """returns True if the object is an instance of a class
     that inherited (directly or indirectly) from the specified class,
