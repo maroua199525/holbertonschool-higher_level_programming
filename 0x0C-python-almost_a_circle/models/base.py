0908ba82add8949838086490e65a52833082ccca
maroua199525 <2646@holbertonschool.com>
2021-06-08 15:35:54 +0100

    almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,9 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """
+            Return A JSON STRING a representation list_dict..
+        """
         if list_dictionaries is None:
             return "[]"
         else:
@@ -23,6 +26,9 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """
+            Write Json Representation of String
+        """
         lst = []
         with open(cls.__name__ + ".json", "w", encoding="UTF-8") as f:
             if list_objs is not None:
@@ -31,6 +37,7 @@ class Base:
             f.write(cls.to_json_string(d))
     @staticmethod
     def from_json_string(json_string):
+
         """ returns the list of the JSON string representation json_string
         """
         if (json_string == None or json_string == ""):
@@ -38,6 +45,7 @@ class Base:
         return json.loads(json_string)
     @classmethod
     def create(cls, **dictionary):
+        
         """ function  that returns an instance with all attributes already set"""
         Square(size):
-        
+
