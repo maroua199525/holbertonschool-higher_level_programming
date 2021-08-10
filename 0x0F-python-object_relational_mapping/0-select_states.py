1cbd729d5396720fda4d55122d385b5384edb9eb
maroua199525 <2646@holbertonschool.com>
2021-08-10 17:04:31 +0100

    first commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" a script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+def my_function():
+    """Open database connection"""
+    db = MySQLdb.connect("localhost", port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    """prepare a cursor object using cursor() method"""
+    cursor = db.cursor()
+    """execute SQL query using execute() method."""
+    cursor.execute("SELECT * from states ORDER BY states.id")
+    result = cursor.fetchall()
+    for state in result:
+        print(state)
+    db.close()
+
+if __name__ == "__main__":
+    my_function()
