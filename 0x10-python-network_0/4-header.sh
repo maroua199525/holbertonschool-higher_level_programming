45c5e8b7fa6503ebafea1a16c8eeef14c27840fd
maroua199525 <2646@holbertonschool.com>
2021-08-19 20:28:51 +0100

    add chmod

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a request to that URL with header variable
-curl -s -X GET "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
